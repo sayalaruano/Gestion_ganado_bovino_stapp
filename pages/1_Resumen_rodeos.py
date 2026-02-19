0c0690f3916a120efc6d4f2c55a5189432639a06
sayalaruano <sebasar1245@gmail.com>
2026-02-19 16:02:28 -0500

    🐛 Fix: add return statment in Home function and add validation for dataset in resumen_rodeos page

--- pages/1_Resumen_rodeos.py
+++ pages/1_Resumen_rodeos.py
@@ -14,18 +14,22 @@ st.set_page_config(
 )
 
 # Adjuntar estilo css personalizado
-with open('style.css') as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+with open("style.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
 # Agregar un título e información sobre la app
-st.title('App para Gestión de Ganado Bovino')
+st.title("App para Gestión de Ganado Bovino")
 
 # Crear un resumen de los datos
-st.subheader('Número de vacas por rodeo')
+st.subheader("Número de vacas por rodeo")
 
 # Obtener el numero de vacas por Rodeo
-numero_vacas_por_rodeo = st.session_state.lista_completa_vacas.groupby('Rodeo')['NumeroRP'].count().reset_index()
-numero_vacas_por_rodeo.columns = ['Rodeo', 'Numero_vacas']
+numero_vacas_por_rodeo = (
+    st.session_state.lista_completa_vacas.groupby("Rodeo")["NumeroRP"]
+    .count()
+    .reset_index()
+)
+numero_vacas_por_rodeo.columns = ["Rodeo", "Numero_vacas"]
 
 # Resetear el indice
 numero_vacas_por_rodeo.reset_index(drop=True, inplace=True)
@@ -33,80 +37,108 @@ numero_vacas_por_rodeo.reset_index(drop=True, inplace=True)
 # Mostrar el dataframe
 st.data_editor(
     numero_vacas_por_rodeo,
-    width = 1000,
+    width=1000,
     column_config={
         "widgets": st.column_config.TextColumn(
             f"Resumen de vacas por rodeo",
-            help= f"Resumen de vacas por rodeo",
+            help=f"Resumen de vacas por rodeo",
         )
     },
     hide_index=True,
 )
 
 # Resumen de ganado  por rodeo
-st.subheader('Resumen de ganado por rodeo')
+st.subheader("Resumen de ganado por rodeo")
 # Añadir un selectbox para seleccionar el rodeo, incluyendo las vacas vendidas y muertas
 rodeo_seleccionado = st.selectbox(
-    ' ',
-    numero_vacas_por_rodeo['Rodeo'].unique(),
+    " ",
+    numero_vacas_por_rodeo["Rodeo"].unique(),
     index=None,
     placeholder="Selecciona el rodeo",
-    label_visibility='collapsed',
+    label_visibility="collapsed",
 )
 
 # Mostrar el dataframe filtrado por el rodeo seleccionado
 st.data_editor(
-    st.session_state.lista_completa_vacas[st.session_state.lista_completa_vacas['Rodeo'] == rodeo_seleccionado].reset_index(drop=True),
-    width = 1500,
+    st.session_state.lista_completa_vacas[
+        st.session_state.lista_completa_vacas["Rodeo"] == rodeo_seleccionado
+    ].reset_index(drop=True),
+    width=1500,
     column_config={
         "widgets": st.column_config.TextColumn(
             f"Resumen de vacas por {rodeo_seleccionado}",
-            help= f"Resumen de vacas por {rodeo_seleccionado}",
+            help=f"Resumen de vacas por {rodeo_seleccionado}",
         )
     },
     hide_index=True,
 )
 
-# Crear bar plot con las edades de las vacas por rodeo seleccionado
-st.subheader('Edades de las vacas por rodeo')
+# Verificar si un rodeo fue seleccionado antes de mostrar plots
+if rodeo_seleccionado:
 
-# Obtener el numero de vacas por Rodeo
-edades_por_rodeo = st.session_state.lista_completa_vacas[st.session_state.lista_completa_vacas['Rodeo'] == rodeo_seleccionado].groupby('Años')['NumeroRP'].count().reset_index()
+    # Crear bar plot con las edades de las vacas por rodeo seleccionado
+    st.subheader("Edades de las vacas por rodeo")
 
-# Renombrar las columnas
-edades_por_rodeo.columns = ['Años', 'Numero_vacas']
-
-barplot_edades = px.bar(edades_por_rodeo, x="Años", y ="Numero_vacas", opacity=0.8, 
-                           color_discrete_sequence=px.colors.qualitative.Plotly)
-
-barplot_edades.update_layout(
-    xaxis=dict(title='Años', tickfont=dict(size=18), titlefont=dict(size=20)),
-    yaxis=dict(title='Numero de vacas', tickfont=dict(size=18), titlefont=dict(size=20))
-    ).update_xaxes(
-        showgrid=False
-    ).update_yaxes(
-        showgrid=False)
-
-st.plotly_chart(barplot_edades, use_container_width=True)
-
-# Crear pie plot con las razas por rodeo seleccionado
-st.subheader('Razas por rodeo')
-# Obtener el numero de vacas por Rodeo
-razas_por_rodeo = st.session_state.lista_completa_vacas[st.session_state.lista_completa_vacas['Rodeo'] == rodeo_seleccionado].groupby('Raza')['NumeroRP'].count().reset_index()
-razas_por_rodeo.columns = ['Raza', 'Numero_vacas']
-
-pie_plot_razas = px.pie(values=razas_por_rodeo['Numero_vacas'],
-                names=razas_por_rodeo['Raza'], 
-                opacity=0.8, color_discrete_sequence=px.colors.qualitative.Plotly)
-
-pie_plot_razas.update_traces(
-    textposition='inside',
-    textinfo='value',
-    insidetextfont=dict(size=18)
-    ).update_layout(
-        legend_title=dict(text='Raza', font=dict(size=24)),
-        legend=dict(font=dict(size=20))
+    # Obtener el numero de vacas por Rodeo
+    edades_por_rodeo = (
+        st.session_state.lista_completa_vacas[
+            st.session_state.lista_completa_vacas["Rodeo"] == rodeo_seleccionado
+        ]
+        .groupby("Años")["NumeroRP"]
+        .count()
+        .reset_index()
     )
 
-st.plotly_chart(pie_plot_razas, use_container_width=True)
-    
+    # Renombrar las columnas
+    edades_por_rodeo.columns = ["Años", "Numero_vacas"]
+
+    barplot_edades = px.bar(
+        edades_por_rodeo,
+        x="Años",
+        y="Numero_vacas",
+        opacity=0.8,
+        color_discrete_sequence=px.colors.qualitative.Plotly,
+    )
+
+    barplot_edades.update_layout(
+        xaxis=dict(title="Años", tickfont=dict(size=18), titlefont=dict(size=20)),
+        yaxis=dict(
+            title="Numero de vacas", tickfont=dict(size=18), titlefont=dict(size=20)
+        ),
+    ).update_xaxes(showgrid=False).update_yaxes(showgrid=False)
+
+    st.plotly_chart(barplot_edades, use_container_width=True)
+
+    # Crear pie plot con las razas por rodeo seleccionado
+    st.subheader("Razas por rodeo")
+    # Obtener el numero de vacas por Rodeo
+    razas_por_rodeo = (
+        st.session_state.lista_completa_vacas[
+            st.session_state.lista_completa_vacas["Rodeo"] == rodeo_seleccionado
+        ]
+        .groupby("Raza")["NumeroRP"]
+        .count()
+        .reset_index()
+    )
+    razas_por_rodeo.columns = ["Raza", "Numero_vacas"]
+
+    pie_plot_razas = px.pie(
+        values=razas_por_rodeo["Numero_vacas"],
+        names=razas_por_rodeo["Raza"],
+        opacity=0.8,
+        color_discrete_sequence=px.colors.qualitative.Plotly,
+    )
+
+    pie_plot_razas.update_traces(
+        textposition="inside", textinfo="value", insidetextfont=dict(size=18)
+    ).update_layout(
+        legend_title=dict(text="Raza", font=dict(size=24)),
+        legend=dict(font=dict(size=20)),
+    )
+
+    st.plotly_chart(pie_plot_razas, use_container_width=True)
+
+else:
+    st.info(
+        "Por favor, selecciona un rodeo arriba para ver el resumen de ganado por rodeo, edades y razas."
+    )
--- 🏠_Home.py
+++ 🏠_Home.py
@@ -16,105 +16,140 @@ st.set_page_config(
 )
 
 # Adjuntar estilo css personalizado
-with open('style.css') as f:
-    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+with open("style.css") as f:
+    st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
 
 # Función para cargar los datos
 def load_data_gsheets(worksheet_name):
-    
+
     # Crear un objeto de conexión
     conn = st.connection("gsheets", type=GSheetsConnection)
 
     if worksheet_name == "Lista_vacas":
         file = conn.read(
-        worksheet=worksheet_name,
-        ttl = 0,
-        usecols = list(range(17)),
-    )
+            worksheet=worksheet_name,
+            ttl=0,
+            usecols=list(range(17)),
+        )
         # Convertir NumeroRP a string
-        file['NumeroRP'] = file['NumeroRP'].astype(str)
-        
+        file["NumeroRP"] = file["NumeroRP"].astype(str)
+
         # Delete rows with nan values in all columns
-        file.dropna(how='all', inplace=True)
-        
+        file.dropna(how="all", inplace=True)
+
         # Delete rows with "nan" string in specific columns
-        file = file[~file['NumeroRP'].str.contains('nan')]
+        file = file[~file["NumeroRP"].str.contains("nan")]
 
         # Resetear el indice
         file.reset_index(drop=True, inplace=True)
 
         return file
-    
-    elif worksheet_name == "Registro_cambios_ganado" or worksheet_name == "Registro_cambios_leche":
+
+    elif (
+        worksheet_name == "Registro_cambios_ganado"
+        or worksheet_name == "Registro_cambios_leche"
+    ):
         file = conn.read(
             worksheet=worksheet_name,
-            ttl = 0,
-            usecols = list(range(1)),
+            ttl=0,
+            usecols=list(range(1)),
         )
-    
+
+        return file
+
     elif worksheet_name == "Produccion_leche":
         file = conn.read(
             worksheet=worksheet_name,
-            ttl = 0,
-            usecols = list(range(3)),
+            ttl=0,
+            usecols=list(range(3)),
         )
-        
+
         return file
+
 
 # Funcion para calcular la edad de un animal, en años y meses
 # @st.cache_data
 def calculate_age_combined(birthdate):
     today = datetime.today()
-    years = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
+    years = (
+        today.year
+        - birthdate.year
+        - ((today.month, today.day) < (birthdate.month, birthdate.day))
+    )
     months = today.month - birthdate.month - (today.day < birthdate.day)
     if months < 0:
         months += 12
     age_formatted = f"{years} años, {months} meses"
     return years, months, age_formatted
 
+
 # Cargar los datos en el cache de la app. Esto se hará solo una vez y todas las páginas tendrán acceso a los datos
 # Pestaña de lista de vacas
-if 'lista_completa_vacas' not in st.session_state:
+if "lista_completa_vacas" not in st.session_state:
     st.session_state.lista_completa_vacas = load_data_gsheets("Lista_vacas")
-    
-    # Convertir 'Fecha_nacimiento' a datetime 
-    st.session_state.lista_completa_vacas['Fecha_nacimiento'] = pd.to_datetime(st.session_state.lista_completa_vacas['Fecha_nacimiento'], format='mixed')
+
+    # Convertir 'Fecha_nacimiento' a datetime
+    st.session_state.lista_completa_vacas["Fecha_nacimiento"] = pd.to_datetime(
+        st.session_state.lista_completa_vacas["Fecha_nacimiento"], format="mixed"
+    )
 
     # Aplicar la función calculate_age_combined a la columna 'Fecha_nacimiento'
-    st.session_state.lista_completa_vacas['Años'], st.session_state.lista_completa_vacas['Meses'], st.session_state.lista_completa_vacas['Edad'] = zip(*st.session_state.lista_completa_vacas['Fecha_nacimiento'].apply(calculate_age_combined))
+    (
+        st.session_state.lista_completa_vacas["Años"],
+        st.session_state.lista_completa_vacas["Meses"],
+        st.session_state.lista_completa_vacas["Edad"],
+    ) = zip(
+        *st.session_state.lista_completa_vacas["Fecha_nacimiento"].apply(
+            calculate_age_combined
+        )
+    )
 
 # Pestaña de registro de cambios
-if 'registro_cambios_ganado' not in st.session_state:
-    st.session_state.registro_cambios_ganado = load_data_gsheets("Registro_cambios_ganado")
+if "registro_cambios_ganado" not in st.session_state:
+    st.session_state.registro_cambios_ganado = load_data_gsheets(
+        "Registro_cambios_ganado"
+    )
 
-if 'producccion_leche' not in st.session_state:
+if "producccion_leche" not in st.session_state:
     st.session_state.producccion_leche = load_data_gsheets("Produccion_leche")
 
     # Elimina filas donde 'Fecha' es nulo
-    st.session_state.producccion_leche = st.session_state.producccion_leche.dropna(subset=['Fecha'])
+    st.session_state.producccion_leche = st.session_state.producccion_leche.dropna(
+        subset=["Fecha"]
+    )
 
     # Convertir 'Fecha' a datetime
-    st.session_state.producccion_leche['Fecha'] = pd.to_datetime(st.session_state.producccion_leche['Fecha'])
+    st.session_state.producccion_leche["Fecha"] = pd.to_datetime(
+        st.session_state.producccion_leche["Fecha"]
+    )
 
-if 'registro_cambios_leche' not in st.session_state:
-    st.session_state.registro_cambios_leche = load_data_gsheets("Registro_cambios_leche")
+if "registro_cambios_leche" not in st.session_state:
+    st.session_state.registro_cambios_leche = load_data_gsheets(
+        "Registro_cambios_leche"
+    )
 
 # Agregar un título e información sobre la app
-st.title('App para Gestión de Ganado Bovino')
+st.title("App para Gestión de Ganado Bovino")
 
-st.write('''Los datos de esta aplicación son de uso exclusivo de la finca Mata Redonda.''')
+st.write(
+    """Los datos de esta aplicación son de uso exclusivo de la finca Mata Redonda."""
+)
 
-with st.expander('Acerca de esta aplicación'):
-    st.write('''
+with st.expander("Acerca de esta aplicación"):
+    st.write(
+        """
     Esta aplicación fue desarrollada para facilitar la gestión de ganado bovino en la finca Mata Redonda, ubicada en el Carchi, Ecuador.
     
     **Creditos**
     - Desarrollada por [Sebastián Ayala Ruano](https://sayalaruano.github.io/).
     - El código de este proyecto está disponible bajo la [licencia MIT](https://mit-license.org/) en este [repositorio GitHub](https://github.com/sayalaruano/Gestion_ganado_bovino_stapp). Si usas o modificas el códifo fuente de este proyecto, por favor provee las atribuciones correspondientes por el trabajo realizado.
     - Si tienes algún comentario o sugerencia acerca de este proyecto, por favor [crea an issue](https://github.com/sayalaruano/Gestion_ganado_bovino_stapp/issues/new) en el repositorio de GitHub del proyecto.
-      ''')
+      """
+    )
 
-st.subheader('Bienvenido/a!')
-st.info('Mira el resumen del ganado y los rodeos, gestiona los rodeos, o agrega/elimina ganado', icon='👈')
-
-
+st.subheader("Bienvenido/a!")
+st.info(
+    "Mira el resumen del ganado y los rodeos, gestiona los rodeos, o agrega/elimina ganado",
+    icon="👈",
+)
